03035d7ea7ba701acb1c9a411a97145cdf030660
OmarAyman85 <omar.shivy85@gmail.com>
2025-05-15 18:50:12 +0300

    Introducing Reactive form for the login form

--- frontend/Authy/src/app/login/login.component.ts
+++ frontend/Authy/src/app/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {
+  EmailValidator,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginReq: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl(false),
+  });
+
+  onSubmit(): void {
+    const formValue = this.loginReq.value;
+    console.log('THIS IS THE LOGIN REQUEST DETAILS : ');
+    console.log(formValue);
+  }
+}
